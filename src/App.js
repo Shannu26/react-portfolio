9d79ea7999b44be5aab5fb79101906d7858f6e5e
Sasank Tadepalli <saisasank25@gmail.com>
2021-06-26 12:43:13 +0530

    Added Home Section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 
 import BackgroundCircles from "./components/BackgroundCircles/BackgroundCircles";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <BackgroundCircles />
       <section>
-        <h1>Hi</h1>
+        <Home />
       </section>
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,22 @@
+import "./Home.css";
+import ProfilePic from "../../images/ProfilePic-small.png";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="left">
+        <h3>Hello, I'm</h3>
+        <h1>Sasank Tadepalli</h1>
+        <h2>Full Stack Web Developer.</h2>
+        <h2>Programmer.</h2>
+      </div>
+      <div className="right">
+        <div className="img-container">
+          <img src={ProfilePic} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
